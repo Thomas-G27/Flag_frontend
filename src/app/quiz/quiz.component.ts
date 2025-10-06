540ddf7c8936681a7228294d146827dfbcf993ce
Creozz43 <clement.caron@epfedu.fr>
2025-10-06 14:52:11 +0200

    réglages des bugs

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -1,4 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { SettingsService } from "../services/settings.service";
+import { Subscription } from "rxjs";
 
 @Component({
     selector: 'quiz',
@@ -6,9 +8,23 @@ import {Component, OnInit} from '@angular/core';
     styleUrls: ['./quiz.component.scss']
 })
 
-export class QuizComponent implements OnInit {
-    constructor() { }
+export class QuizComponent implements OnInit, OnDestroy {
+    currentLanguage: string = 'fr'
+    translations: any = {}
+    private languageSubscription!: Subscription
+
+    constructor(private settingsService: SettingsService) { }
+
     ngOnInit(): void {
-        
+        this.languageSubscription = this.settingsService.language$.subscribe(lang => {
+            this.currentLanguage = lang
+            this.translations = this.settingsService.getTranslation(lang)
+        })
+    }
+
+    ngOnDestroy(): void {
+        if (this.languageSubscription) {
+            this.languageSubscription.unsubscribe()
+        }
     }
 }
