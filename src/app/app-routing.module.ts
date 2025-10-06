4fefeb5c773ad8a1740124a07cf0a484b06dd18c
Creozz43 <clement.caron@epfedu.fr>
2025-10-06 12:01:12 +0200

    arborescence nettoyée

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,47 +3,11 @@ import { RouterModule, Routes } from "@angular/router"
 import { HomeComponent } from "home/home.component"
 import { QuizComponent } from "quiz/quiz.component"
 import { RulesComponent } from "rules/rules.component"
-import { StudentsComponent } from "students/students.component"
-import { StudentsResolver } from "students/students.resolver"
-import { StudentDetailsComponent } from "students/student-details/student-details.component"
-import { StudentDetailsResolver } from "students/student-details/student-details.resolver"
-import { MajorsComponent } from "majors/majors.component"
-import { MajorsResolver } from "majors/majors.resolver"
-import { MajorStudentsResolver } from "majors/major-students/major-students.resolver"
-import { MajorStudentsComponent } from "majors/major-students/major-students.component"
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "quiz", component: QuizComponent },
   { path: "rules", component: RulesComponent },
-  {
-    path: "etudiants",
-    component: StudentsComponent,
-    resolve: {
-      students: StudentsResolver,
-    },
-  },
-  {
-    path: "details-etudiant/:id",
-    component: StudentDetailsComponent,
-    resolve: {
-      student: StudentDetailsResolver,
-    },
-  },
-  {
-    path: "filieres",
-    component: MajorsComponent,
-    resolve: {
-      majors: MajorsResolver,
-    },
-  },
-  {
-    path: "etudiants-filiere/:id",
-    component: MajorStudentsComponent,
-    resolve: {
-      studentsFromMajor: MajorStudentsResolver,
-    },
-  },
 ]
 
 @NgModule({
