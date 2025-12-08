ff35299d52870b3ee1cd185118c0865b4ecd42c1
ThomasG <thomas.gbt2003@gmail.com>
2025-12-08 14:40:32 +0100

    ajout des deockerfile et docker compose pour tout, tout n'est pas traqué sur git il va falloir un git plus global mais jsp comment faire

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,19 @@
-# http-server/Dockerfile
+FROM node:18-alpine AS builder
+WORKDIR /app
+
+# installer deps et builder l'app Angular
+COPY package*.json ./
+RUN npm ci --silent
+COPY . .
+# utilise le script build de package.json avec la bonne syntaxe ng build
+RUN npm run build -- --output-path=dist
+
 FROM httpd:2.4-alpine
-
-# Copier le code source du front dans le dossier /usr/local/apache2/htdocs/
-COPY src/ /usr/local/apache2/htdocs/
-
-# Copier la conf de reverse proxy dans le dossier conf/extra
+# copier les fichiers construits dans htdocs
+COPY --from=builder /app/dist/ /usr/local/apache2/htdocs/
+# proxy + rewrite config
 COPY proxy.conf /usr/local/apache2/conf/extra/proxy.conf
-
-# S'assurer que httpd.conf inclut notre conf. Si l'image l'inclut déjà, cette ligne est sans effet.
 RUN echo "Include conf/extra/proxy.conf" >> /usr/local/apache2/conf/httpd.conf
 
 EXPOSE 80
+CMD ["httpd-foreground"]
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from "@angular/core"
 import { RouterModule, Routes } from "@angular/router"
-import { HomeComponent } from "home/home.component"
-import { QuizComponent } from "quiz/quiz.component"
-import { QuizChoiceComponent } from "quiz_choice/quiz_choice.component"
-import { RulesComponent } from "rules/rules.component"
-import { CountriesComponent } from "countries/countries.component"
+import { HomeComponent } from "./home/home.component"
+import { QuizComponent } from "./quiz/quiz.component"
+import { QuizChoiceComponent } from "./quiz_choice/quiz_choice.component"
+import { RulesComponent } from "./rules/rules.component"
+import { CountriesComponent } from "./countries/countries.component"
 import { InscriptionComponent } from "./inscription/inscription.component" 
-import { HofComponent } from "Hof/Hof.component"
-import { Hof_adminComponent } from "Hof_admin/Hof_admin.component"
+import { HofComponent } from "./hof/hof.component"
+import { Hof_adminComponent } from "./hof_admin/hof_admin.component"
 import { AuthGuard } from "./auth/auth.guard";
-import { ConnexionComponent } from "connexion/connexion.component"
+import { ConnexionComponent } from "./connexion/connexion.component"
 
 const routes: Routes = [
   { path: "", redirectTo: "/home", pathMatch: "full" },
@@ -20,7 +20,7 @@ const routes: Routes = [
   { path: "quiz/language", component: QuizComponent, canActivate: [AuthGuard] },
   { path: "quiz_choice", component: QuizChoiceComponent, canActivate: [AuthGuard] },
   { path: "rules", component: RulesComponent },
-  { path: "countries", component: CountriesComponent, canActivate: [AuthGuard] },
+  { path: "countries", component: CountriesComponent }, // enlevement pr débug, canActivate: [AuthGuard]
   { path: "inscription", component: InscriptionComponent },
   { path: "connexion", component: ConnexionComponent},
   { path: "hall_of_fame", component: HofComponent, canActivate: [AuthGuard] },
